1ab927ed67a4bf2c7fca06ceef73c5a1e3ab31cf
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-04 21:31:22 +0800

    loop to print alphabets

---
+++ PrintAlphabets/src/printalphabets/PrintAlphabets.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printalphabets;
+
+/**
+ *
+ * @author Ashi
+ */
+public class PrintAlphabets {
+
+    public static void main(String[] args) {
+        for (char ch='a'; ch<= 'z'; ch++){
+            System.out.println(ch+"");
+        }
+    }
+    
+}
+
+//Simple program to print out all alphabets.
