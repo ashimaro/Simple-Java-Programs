0d9e24ef7fa909b7321af2900fb234a35597e6e4
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-10 23:18:47 +0800

    Add comments and review code

--- LinkedListDemo/src/linkedlistdemo/LinkedListDemo.java
+++ LinkedListDemo/src/linkedlistdemo/LinkedListDemo.java
@@ -10,16 +10,18 @@ import java.util.LinkedList;
 public class LinkedListDemo {
 
     public static void main(String[] args) {
+        //create new linked list instance with integer type elements
         LinkedList<Integer> ll = new LinkedList<>();
-
+// add elements
         ll.add(1);
         ll.add(2);
         ll.add(3);
 
         System.out.println(ll);
-
+//create another new linked list elements.
         LinkedList<Integer> ll1 = new LinkedList<>();
-
+//descending iterator to reverse the order.
+//adds each elements to new linked list instance
         ll.descendingIterator().forEachRemaining(ll1::add);
 
         System.out.println(ll1);
