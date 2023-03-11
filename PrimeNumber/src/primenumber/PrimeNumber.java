6d8782f1d7f16a40377be0647699b2bb95674567
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-11 22:09:55 +0800

    Add comments and review code

--- PrimeNumber/src/primenumber/PrimeNumber.java
+++ PrimeNumber/src/primenumber/PrimeNumber.java
@@ -1,40 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package primenumber;
 
 import java.util.Scanner;
 
 /**
- *
- * @author User
+ * The PrimeNumber class contains a main method that takes a positive integer input from the user
+ * and determines whether it is a prime number or not.
+ * 
+ * @author Ashi
  */
 public class PrimeNumber {
 
     public static void main(String[] args) {
         int x;
         
+        // Create a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
+        
+        // Prompt the user to enter a positive integer
         System.out.println(" Enter positive integer :");
+        
+        // Read the integer input from the user
         x = sc.nextInt();
-boolean isPrime = true;
+        
+        // Initialize a boolean variable to track whether the input number is prime
+        boolean isPrime = true;
+        
+        // Check whether the input number is less than 2 (which is not a prime number)
         if (x < 2) {
             isPrime = false;
         } else {
+            // Check whether the input number is divisible by any number between 2 and its square root
             for (int i = 2; i <= Math.sqrt(x); i++) {
                 if (x % i == 0) {
+                    // If the input number is divisible by a number between 2 and its square root, it is not a prime number
                     isPrime = false;
                     break;
                 }
             }
         }
-       if(isPrime){
-           System.out.println(x+" is prime number");
-       }else{
-           System.out.println(x+" is NOT prime number");
-       }
+        
+        // Print the result whether the input number is a prime number or not
+        if(isPrime){
+            System.out.println(x+" is prime number");
+        } else {
+            System.out.println(x+" is NOT prime number");
+        }
 
     }
 }
