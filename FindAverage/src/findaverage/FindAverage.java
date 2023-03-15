e2e5dfe8f94b8306443b0d1de21999b22d286af4
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-15 20:33:06 +0800

    add comment and review code

--- FindAverage/src/findaverage/FindAverage.java
+++ FindAverage/src/findaverage/FindAverage.java
@@ -12,14 +12,17 @@ public class FindAverage {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter number counts :");
+       //declare variable for count
        int count = sc.nextInt();
+       //declare variable for sum up in for loop
        int sum=0;
        for(int i=0;i<count;i++){
+           //the loop will repeat based on count length
            System.out.println("Enter integer :");
            int num = sc.nextInt();
            sum =sum+num;
            
-       }
+       }//end loop
        double average = sum/count;
        System.out.println("Average :"+average);
        
