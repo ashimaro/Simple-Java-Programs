ee0e2cb75f62100fda0752ffa6f52f5ff9aec26a
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-03-19 23:32:35 +0800

    add comment and review code

--- PrintAlphabets/src/printalphabets/PrintAlphabets.java
+++ PrintAlphabets/src/printalphabets/PrintAlphabets.java
@@ -12,6 +12,7 @@ package printalphabets;
 public class PrintAlphabets {
 
     public static void main(String[] args) {
+        //char is used to store a single characters like a,b,x
         for (char ch='a'; ch<= 'z'; ch++){
             System.out.println(ch+"");
         }
