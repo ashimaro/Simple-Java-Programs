4e2662733349df59778b6a8e5d72b64187bbccdb
Ashii99 <124756780+ashimaro@users.noreply.github.com>
2023-05-11 23:14:52 +0800

    Display the multiplication table of a given integer

---
+++ timeTable/src/timetable/TimeTable.java
@@ -0,0 +1,28 @@
+
+package timetable;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ashi
+ */
+public class TimeTable {
+    public static void main(String[] args) {
+        {
+   int j,n;
+
+   System.out.print("Input the number(Table to be calculated): ");
+{
+   System.out.print("Input number of terms : ");
+    Scanner in = new Scanner(System.in);
+		    n = in.nextInt();
+
+   System.out.println ("\n");
+   for(j=0;j<=n;j++)
+  
+     System.out.println(n+" X "+j+" = " +n*j);
+   }
+}
+}
+}
